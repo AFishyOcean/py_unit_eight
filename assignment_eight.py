e85ea40145db9c37b8a888e9ff878b1cddae5d69
AFishyOcean <Oo*2=31.9988>
2021-12-03 08:43:07 -0500

    Worked on assignment eight

---
+++ assignment_eight.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Calculator')
+#variables
+
+
+#operations
+add = tk.Button(root, text="+", )
+add.grid(row=, column=, columnspan=)
+
+subtract = tk.Button(root, text="-", )
+subtract.grid(row=, column=, columnspan=)
+
+multiply = tk.Button(root, text="x", )
+multiply.grid(row=, column=, columnspan=)
+
+divide = tk.Button(root, text="/", )
+divide.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="=", command=)
+calculate.grid(row=, column=, columnspan=)
+
+#numbers
+calculate = tk.Button(root, text="0", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="1", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="2", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="3", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="4", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="5", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="6", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="7", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="8", command=)
+calculate.grid(row=, column=, columnspan=)
+
+calculate = tk.Button(root, text="9", command=)
+calculate.grid(row=, column=, columnspan=)
+
+
+
+
+
+root.mainloop()
---
+++ temp_converter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Temp Converter')
+
+
+
+
+
+root.mainloop()
